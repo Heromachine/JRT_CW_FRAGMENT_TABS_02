7320dec18a376877460068b7292ccaa8b7818df5
herotruth <jesse.reyna01@utrgv.edu>
2018-09-02 17:41:24 -0500

    All Files Copied

--- app/src/main/java/com/example/herotruth/jrt_cw_fragment_tabs_02/MainActivity.java
+++ app/src/main/java/com/example/herotruth/jrt_cw_fragment_tabs_02/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.herotruth.jrt_cw_fragment_tabs_02;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
 
+
+
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+//
+//HELLO TESTING
+
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    Toolbar toolBar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolBar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.pager);
+
+        setSupportActionBar(toolBar);
+
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        tabLayout.setOnTabSelectedListener(this);
+
+        Pager myPager = new Pager
+                (
+                        getSupportFragmentManager(),
+                        tabLayout.getTabCount()
+                );
+
+        //Adding adapter to pager
+        viewPager.setAdapter(myPager);
+
+        // When swiping between pages, select the
+        // corresponding tab.
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                //actionBar.setSelectedNavigationItem(postion);
+                tabLayout.setScrollPosition(position,0,true);
+                tabLayout.setSelected(true);
+
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+
+
+
     }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+
 }
